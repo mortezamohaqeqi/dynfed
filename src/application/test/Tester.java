07caede22a5cfcdb52163dc2907d86dba1ceab13
Morteza <morteza.mohagheghy@gmail.com>
2021-06-15 09:47:26 +0200

    Rename DACExp1 to RTCSA_Exp1

--- src/application/test/Tester.java
+++ src/application/test/Tester.java
@@ -2,14 +2,14 @@ package application.test;
 
 import application.common.structs.ExpResult;
 import application.simengine.ExperimentManagerNoGUI;
-import application.test.examples.DACExp1;
+import application.test.examples.RTCSA_Exp1;
 import application.test.examples.IExperiment;
 
 public class Tester 
 {	
 	public static void main(String[] args)  
 	{		
-		IExperiment exp = new DACExp1();
+		IExperiment exp = new RTCSA_Exp1();
 		
 		ExperimentManagerNoGUI runner = new ExperimentManagerNoGUI();
 		ExpResult res = runner.syncRun(exp.simParam(), exp.schParams());
